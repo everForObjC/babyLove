4d5db3a96148c6c9ccb0e8050dcf33cbf4e2f409
everForObjC <apple@appletekiMacBook-Air.local>
2016-08-25 19:40:36 +0800

    第一次
    创次

---
+++ babyLove/babyLove/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  babyLove
+//
+//  Created by apple on 16/8/25.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    UILabel * label = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    label.backgroundColor = [UIColor redColor];
+    [self.view addSubview:label];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
